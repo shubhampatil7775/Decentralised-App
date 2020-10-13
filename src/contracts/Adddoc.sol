1dbd60340b584a62f6e3fdda626a7bec9c20005d
shubhampatil7775 <shubhampatil.sp7775@gmail.com>
2020-10-13 18:11:13 +0530

    atom

--- src/contracts/Adddoc.sol
+++ src/contracts/Adddoc.sol
@@ -13,7 +13,7 @@ contract Adddoc {
     address [] public User;
     mapping(address => UserInfo) UserMap;
 
-       
+
 
     function set(string memory _memeHash) public {
         memeHash = _memeHash;
@@ -22,18 +22,18 @@ contract Adddoc {
     function get() public view returns (string memory) {
         return memeHash;
     }
-    
+
 
     function AddUser(address UserAddress,string memory FullName,string memory EmailID,string memory MobileNo) public
-    {   
+    {
         UserMap[UserAddress]=UserInfo(FullName,EmailID,MobileNo);
         User.push(UserAddress);
     }
-    
+
     function getUsers() view public returns (address[] memory) {
         return User;
     }
-    
+
     function getUser(address ins) view public returns (string memory, string memory, string memory) {
         return (UserMap[ins].FullName,UserMap[ins].EmailID,UserMap[ins].MobileNo );
     }
